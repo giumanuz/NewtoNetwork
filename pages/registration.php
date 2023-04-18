89155f44849dd28c0ba033d930321a4bd34b22ba
giulio <giuliomanuzzi@gmail.com>
2023-04-18 13:52:07 +0200

    ok

--- pages/registration.php
+++ pages/registration.php
@@ -4,17 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../styles/login.css">
-  <!-- <script src="https://code.jquery.com/jquery-2.2.1.js" integrity="sha256-eNcUzO3jsv0XlJLveFEkbB8bA7/CroNpNVk3XpmnwHc=" crossorigin="anonymous"></script>
-  <script>
-    $.get("navigationBar.html", function(data){
-      $("#nav-placeholder").replaceWith(data);
-    });
-  </script>
-  <script src="../script/registration.js"></script> -->
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-   -->
+
 
   <title>
     Registration
