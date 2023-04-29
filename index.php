bf405bd649d0a79eca97666434608a64d42f163d
Matteo D'Agostino <dagos.matteo@gmail.com>
2023-04-29 19:44:39 +0200

    centered the modal for post creation

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="/css/index.css">
 </head>
 <div class="modal fade" id="modalPost" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">New Post</h1>
