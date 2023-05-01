5b13b802bb7e0a8310a9868f7995d1d6d41488d6
giulio <giuliomanuzzi@gmail.com>
2023-05-01 16:44:10 +0200

    added notification friend

--- script/backPost.php
+++ script/backPost.php
@@ -15,44 +15,22 @@
     $line = pg_fetch_array($result, null, PGSQL_ASSOC);
     $post_id = $line['count'] + 1;
 
-    // $target_file = realpath(dirname(getcwd()));
-    // $target_file = $target_file . "/download/" . basename($_FILES["photo"]["name"]);
-    // $uploadOk = 1;
-    // $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // // Check if image file is a actual image
-    // $check = getimagesize($_FILES["photo"]["tmp_name"]);
+    $photoToUpload = $_FILES["photo"]["tmp_name"];
+    $photoToUpload = base64_encode(file_get_contents(addslashes($photoToUpload)));
+    $query2 = "INSERT INTO posts (post_id, writer, post_content, photo) VALUES ($post_id, '$writer', '$post_content', '$photoToUpload')";
+    if( $result = pg_query($dbconnession, $query2)){
 
-    // if($check === false) {
-    //     header("Location: /pages/createPost.php?status=errorFakeImage");
-    //     $uploadOk = 0;
-    // }
+        $query3 = "SELECT * FROM friends where user_id = $1";
+        $result3 = pg_query_params($dbconnession, $query3, array($writer)) or die("Query failed: " . pg_last_error());
+        while( $line3 = pg_fetch_array($result3, null, PGSQL_ASSOC)) {
+            $friend = $line3['friend_user_id'];
+            $query4 = "INSERT INTO notifications (user_from, user_to, notification_content) VALUES ($1, $2, $3)";
+            $result4 = pg_query_params($dbconnession, $query4, array($writer, $friend, "has written a new posts.")) or die("Query failed: " . pg_last_error());
+        }
 
-    // if (file_exists($target_file)) {
-    //     header("Location: /pages/createPost.php?status=errorFileExists");
-    //     $uploadOk = 0;
-    // }
 
-    // if ($_FILES["photo"]["size"] > 500000) {
-    //     header("Location: /pages/createPost.php?status=errorSize");
-    //     $uploadOk = 0;
-    // }
+        header("Location: /index.php");
+    }else
+        header("Location: /pages/createPost.php?status=errorUpload");
 
-    // if($imageFileType != "jpg" && $imageFileType != "jpeg") {
-
-    //     header("Location: /pages/createPost.php?status=errorFileFormat");
-    //     $uploadOk = 0;
-    // }
-    // Check if $uploadOk is set to 0 by an error
-    // if everything is ok, try to upload file
-    // if ($uploadOk == 1) {
-        // if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-        $photoToUpload = $_FILES["photo"]["tmp_name"];
-        $photoToUpload = base64_encode(file_get_contents(addslashes($photoToUpload)));
-        $query2 = "INSERT INTO posts (post_id, writer, post_content, photo) VALUES ($post_id, '$writer', '$post_content', '$photoToUpload')";
-        if( $result = pg_query($dbconnession, $query2))
-            header("Location: /index.php");
-         else
-            header("Location: /pages/createPost.php?status=errorUpload");
-        // }
-    // 
 ?>
