e3ddc4d56d0e1109365cdabcae3b04d90da7bd69
giulio <giuliomanuzzi@gmail.com>
2023-05-03 13:13:27 +0200

    fixed requests

--- script/acceptRequest.php
+++ script/acceptRequest.php
@@ -11,10 +11,10 @@
     $reciver = $_SESSION['username'];
     $query = "DELETE FROM friend_requests WHERE sender = $1 AND reciver = $2";
     $result = pg_query_params($dbconnession, $query, array($sender, $reciver)) or die("Query failed: " . pg_last_error());
+
     $query = "INSERT INTO friends (user_id, friend_user_id) VALUES ($1, $2)";
     $result = pg_query_params($dbconnession, $query, array($reciver, $sender)) or die("Query failed: " . pg_last_error());
-    $query = "INSERT INTO friends (user_id, friend_user_id) VALUES ($1, $2)";
-    $result = pg_query_params($dbconnession, $query, array($sender, $reciver)) or die("Query failed: " . pg_last_error());
+
     $query = "INSERT INTO notifications (user_from, user_to, notification_content) VALUES ($1, $2, $3)";
     $result = pg_query_params($dbconnession, $query, array($reciver, $sender, "accepted your friend request")) or die("Query failed: " . pg_last_error());
     
