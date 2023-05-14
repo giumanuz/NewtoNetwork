7482578c2967c05e8d81efe4b459df92945e1445
giulio <giuliomanuzzi@gmail.com>
2023-05-14 23:28:40 +0200

    fixed bugs

--- js/requestHandler.js
+++ js/requestHandler.js
@@ -14,25 +14,53 @@ document.querySelectorAll('.btn-decline').forEach(button => {
 });
 
 function acceptRequest(username) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-      }
-    };
-    xhttp.open("GET", "/script/acceptRequest.php?sender=" + username, true);
-    xhttp.send();
-    window.location.reload();
+
+
+
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //   if (this.readyState == 4 && this.status == 200) {
+    //     console.log(this.responseText);
+    //   }
+    // };
+    // xhttp.open("GET", "/script/acceptRequest.php?sender=" + username, true);
+    // xhttp.send();
+    // window.location.reload();
+
+    $.ajax({
+        url: '/script/acceptRequest.php',
+        type: 'GET',
+        data: {
+            sender: username
+        },
+        success: function(response) {
+            console.log(response);
+            window.location.reload();
+        }
+    });
 }
 
 function discardRequest(username) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-      }
-    };
-    xhttp.open("GET", "/script/discardRequest.php?sender=" + username, true);
-    xhttp.send();
-    window.location.reload();
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //   if (this.readyState == 4 && this.status == 200) {
+    //     console.log(this.responseText);
+    //   }
+    // };
+    // xhttp.open("GET", "/script/discardRequest.php?sender=" + username, true);
+    // xhttp.send();
+    // window.location.reload();
+
+    $.ajax({
+        url: '/script/discardRequest.php',
+        type: 'GET',
+        data: {
+            sender: username
+        },
+        success: function(response) {
+            console.log(response);
+            window.location.reload();
+        }
+    });
+    
   }
