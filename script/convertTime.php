1e4840e56f7cf1fb4dd4543a685260a535dc6487
giulio <giuliomanuzzi@gmail.com>
2023-05-28 18:40:32 +0200

    done

--- script/convertTime.php
+++ script/convertTime.php
@@ -3,7 +3,7 @@
 
     function convertTime($time){ 
         $time=strtotime($time);
-        $time = time() - $time; //+ 7200;
+        $time = time() - $time  + 7200;     //qui modifica
         $time = ($time<1)? 1 : $time;
         $tokens = array (
             31536000 => 'year',
